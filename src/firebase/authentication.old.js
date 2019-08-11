72dc16d92eecbf19c472e08c4e5dfc43a2b21322
unoctanium <odo@maletzki.net>
2019-08-11 14:41:37 +0200

    Code cleaning part 1

--- src/firebase/authentication.old.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase/app'
-
-import store from '@/store'
-import { isNull } from "@/util"
-
-firebase.auth().onAuthStateChanged(firebaseUser => {
-  const actionToDispatch = (!isNull(firebaseUser)) ? 'signedin' : 'signedout'
-  store.dispatch(`auth/${actionToDispatch}`, firebaseUser)
-})
